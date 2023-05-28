49436c0891879566ea9c0ad0a0cb1bc1cb0262c5
Ibnusabil43 <zaidan.697@gmail.com>
2023-05-28 09:41:35 +0700

    firstcommit

--- Server_beta.py
+++ Server_beta.py
@@ -15,15 +15,16 @@ DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 def http_response(file):
     response = ''  # inisisasi response
 
-    # mengambil filename dari request dengan metode split
-    filename = file.split(' ')[1]
+    # mengambil filename dari hasil parsing dengan metode split
+    filename = file.split()[1][1:]
 
     # menentukan filepath dengan menggabungkan base directory dengan filename menggunakan metode join
-    if filename == "/":  # Jika memasukkan url tanpa filename maka akan membuka file_html.html
-        filename = "/index.html"
-    filepath = DIRECTORY + filename
-    try:  # pengkondisian jika file ditemukan
+    if filename == "":  # Jika memasukkan url tanpa filename maka akan membuka file_html.html
+        filename = "index.html"
 
+    #menggabungkan Directory file py berapa dengan filename yang sudah kita split dari hasil parsing
+    filepath = os.path.join(DIRECTORY, filename)
+    if os.path.isfile(filepath): # pengkondisian jika file ditemukan
         # membuka file dengan metode read binary
         content = open(filepath, 'rb').read()
 
@@ -32,7 +33,7 @@ def http_response(file):
             'utf-8')
         response += content
 
-    except FileNotFoundError or IndexError:  # pengkondisian jika file tidak ditemukan
+    else:  # pengkondisian jika file tidak ditemukan
 
         # Response message jika file tidak ditemukan atau file tidak dapat dibuka
         response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html; charset=utf-8\r\n\r\n".encode(
@@ -48,35 +49,33 @@ server_socket = socket(AF_INET, SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 
 # Tunggu Koneksi masuk
-server_socket.listen(2)
+server_socket.listen()
 
 # Pesan konfirmasi bahwa server telah berjalan
 print(f"Server berjalan di {HOST} port {PORT} (http://{HOST}:{PORT})......")
 
 # Perulangan agar web server dapat selalu melayani client
 while True:
-   # Terima koneksi server
+    # Terima koneksi server
     connnectionSocket, addr = server_socket.accept()
 
     # Baca data yang dikirimkan client (parsing)
-    file = connnectionSocket.recv(102400).decode()
+    parse = connnectionSocket.recv(102400).decode()    
 
     # memanggil fungsi http response untuk menampilkan http respons
-    http_respons = http_response(file)
+    http_respons = http_response(parse)
 
     # print method path dan protocol yang terdapat pada variabel request
-    method, path, protocol = file.split('\n')[0].split()
+    method, path, protocol = parse.split('\n')[0].split()
 
     # pesan konfirmasi bahwa file berhasil dibuka
-    print("FILE BERHASIL DIBUKA !!")
     print('Method :', method)
-    filepath = os.path.join(DIRECTORY, file.split()[1][1:])
-    print("Directory File .py : ", DIRECTORY)
+    filepath = os.path.join(DIRECTORY, parse.split()[1][1:])
     print('Path :', filepath)
     print('Protocol: ', protocol)
 
     # kirim response ke client
     connnectionSocket.sendall(http_respons)
 
-    # tutup koneksi
+    # Tutup Koneksi Client
     connnectionSocket.close()
